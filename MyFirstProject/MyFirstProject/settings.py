67f41158189a9640f47b8e1131946bcb60cf9271
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-20 11:32:56 +0545

    Initial

--- MyFirstProject/MyFirstProject/settings.py
+++ MyFirstProject/MyFirstProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!#(fba#bn6u+c0dg2glnv6x94%zmvh7a_l@hrlw!k1&jzt_b3w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
