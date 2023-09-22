071fb647afc56d1055fb159374c2059be3a83cd8
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-22 14:51:22 +0545

    Initial

--- MyFirstProject/MyFirstProject/views.py
+++ MyFirstProject/MyFirstProject/views.py
@@ -3,3 +3,5 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse("<h1> Welcome the Home Page</h1>")
 
+def DynamicUrl(request):
+    return HttpResponse("<h1> Dynamic URL Page</h1>")
