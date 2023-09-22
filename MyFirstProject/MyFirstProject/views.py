1748cac152b8127c13e32084a1e6c1aee00e9c92
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-22 10:40:28 +0545

    Initial

--- MyFirstProject/MyFirstProject/views.py
+++ MyFirstProject/MyFirstProject/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
 
-# def home(request):
-#     return HttpResponse("<h1> Welcome the Home Page</h1>")
+def home(request):
+    return HttpResponse("<h1> Welcome the Home Page</h1>")
 
