bc8f9baf67321d2eb8308e5169772e7b75342292
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-22 10:29:16 +0545

    Initial

--- MyFirstProject/MyFirstProject/views.py
+++ MyFirstProject/MyFirstProject/views.py
@@ -17,7 +17,7 @@ def about():
     <title>Santosh Bhandari | About Us</title>
   </head>
   <body>
-    <h1>مرحبًا بالعالم!</h1>
+    <h1 class="text-center">About Us</h1>
 
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
