3e35841de6f9cdeacdea35acb1fe4e734f52b2d0
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-22 15:41:37 +0545

    Initial

--- MyFirstProject/MyFirstProject/views.py
+++ MyFirstProject/MyFirstProject/views.py
@@ -4,12 +4,12 @@ from django.shortcuts import render
 def home(request):
     return render(request,"index.html")
 
-def dynamic(request,data):
-    return HttpResponse(data)
-    # datas={
-    #     'value':data
-    # }
-    # return render(request,"other.html",datas)
+def dynamic(request,url):
+    # return HttpResponse(url)
+    datas={
+        'value':url
+    }
+    return render(request,"other.html",datas)
 
 # def DynamicUrl(request):
 #     return HttpResponse("<h1> Dynamic URL Page</h1>")
