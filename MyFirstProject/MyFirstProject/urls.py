bc4cd7819f52760e0f29b3e33cb7ac418baf9bd4
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-22 15:40:52 +0545

    Initial

--- MyFirstProject/MyFirstProject/urls.py
+++ MyFirstProject/MyFirstProject/urls.py
@@ -20,7 +20,8 @@ from MyFirstProject import views
 
 urlpatterns = [
     path('',views.home,name='home'),
-    path('home/',views.home,name='home')
+    path('home/',views.home,name='home'),
+    path('<str:url>',views.dynamic)
 
 
 
