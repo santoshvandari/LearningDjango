9a5d5fb841a3e6b08800cb868e85a8d273efe9d6
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-22 18:28:57 +0545

    Initial

--- MyFirstProject/static/js/script.js
+++ MyFirstProject/static/js/script.js
@@ -1,5 +1,4 @@
 const color = (min,max)=> Math.floor(Math.random()*(max-min)+min)
-GeneratedColor=`rgb(${color(0,255)},${color(0,255)},${color(0,255)})`
 setInterval(()=>{
-    document.querySelector(".main").style.backgroundColor=GeneratedColor
+    document.querySelector(".main").style.backgroundColor=`rgb(${color(0,255)},${color(0,255)},${color(0,255)})`
 },100)
