fb83f27abb6a2227ac8e2a9619224057ce239d39
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-22 10:38:54 +0545

    Initial

--- MyFirstProject/MyFirstProject/views.py
+++ MyFirstProject/MyFirstProject/views.py
@@ -1,41 +1,5 @@
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("<h1> Welcome the Home Page</h1> <a href='/about'>About Me</a>")
+# def home(request):
+#     return HttpResponse("<h1> Welcome the Home Page</h1>")
 
-def about(request):
-    return HttpResponse('''<!doctype html>
-<html lang="ar" dir="rtl">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.rtl.min.css" integrity="sha384-nU14brUcp6StFntEOOEBvcJm4huWjB0OcIeQ3fltAfSmuZFrkAif0T+UtNGlKKQv" crossorigin="anonymous">
-
-    <title>Santosh Bhandari | About Us</title>
-  </head>
-  <body>
-    <h1 class="text-center">About Us</h1>
-    <form class="col-6 text-center m-auto">
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-  </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-
-  </body>
-</html>''')
