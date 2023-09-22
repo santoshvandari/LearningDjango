7420a07a9fee0bf22b6ecc9780b9041f0b680c56
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-22 10:25:53 +0545

    Initial

--- MyFirstProject/MyFirstProject/urls.py
+++ MyFirstProject/MyFirstProject/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from MyFirstProject import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.home,name='home'),
+    path('home',views.home,name='home')
 ]
 
 admin.site.site_header = "Santosh Login"
