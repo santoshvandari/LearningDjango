5cd05d958f7cdec2c3adfdd3dfdecbf1c4629bf4
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-23 19:23:09 +0545

    Initial

--- MyFirstProject/MyFirstProject/form.py
+++ MyFirstProject/MyFirstProject/form.py
@@ -3,4 +3,5 @@ from django import forms
 class UserDataForm(forms.Form):
     name=forms.CharField(label="Name",required=False, widget=forms.TextInput)
     address=forms.CharField(label="Address",required=False, widget=forms.TextInput)
+    email=forms.EmailField(label="Email")
     msg=forms.CharField(label="Message",required=False,widget=forms.Textarea)
