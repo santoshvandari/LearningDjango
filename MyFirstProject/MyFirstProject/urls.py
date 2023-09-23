7f16b97f64f08dbcdd1a15e73c0b974183a7e644
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-23 18:44:58 +0545

    Initial

--- MyFirstProject/MyFirstProject/urls.py
+++ MyFirstProject/MyFirstProject/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('services/',views.services),
     path('about/',views.about),
     path('form/',views.form,name='form'),
-    path('submit/',views.form,name='submit'),
+    path('submit/',views.submit,name='submit'),
 
 
 
