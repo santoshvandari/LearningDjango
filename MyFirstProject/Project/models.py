7d6eca2e854f6aa7b8e5dbe2c510be1c787f5313
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-24 15:15:42 +0545

    Initial

--- MyFirstProject/Project/models.py
+++ MyFirstProject/Project/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class ContactData(models.Model):
+    name=models.CharField(max_length=30)
+    email=models.CharField(max_length=30)
+    message=models.TextField()
+
+    class Meta:
+        db_table="ContactData"
