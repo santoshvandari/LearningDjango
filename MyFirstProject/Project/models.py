76ca6368171eee00b70090d31c6d287f28d5bda0
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-24 19:20:12 +0545

    Initial

--- MyFirstProject/Project/models.py
+++ MyFirstProject/Project/models.py
@@ -15,7 +15,5 @@ class FormData(models.Model):
     name=models.CharField(max_length=30)
     email=models.CharField(max_length=30)
     message=models.TextField()
-    datetime = models.DateTimeField(auto_now_add = True)
     class Meta:
-        db_table="ContactData"
-        get_latest_by = "datetime"\
+        db_table="FormData"
