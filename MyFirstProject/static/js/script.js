42b50d9f4f375a5edec9824ca4e259765cb40839
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-24 19:52:08 +0545

    Initial

--- MyFirstProject/static/js/script.js
+++ MyFirstProject/static/js/script.js
@@ -2,3 +2,6 @@ const color = (min,max)=> Math.floor(Math.random()*(max-min)+min)
 setInterval(()=>{
     // document.querySelector(".main").style.backgroundColor=`rgb(${color(0,255)},${color(0,255)},${color(0,255)})`
 },100)
+
+document.querySelector(".message")
+console.log("connected.")
