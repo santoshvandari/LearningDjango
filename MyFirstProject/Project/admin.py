a27c8319253c8d89ff96a9bdc539b9fbdd2f59d4
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-24 19:41:42 +0545

    Initial

--- MyFirstProject/Project/admin.py
+++ MyFirstProject/Project/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from Project.models import ContactData
+from Project.models import ContactData,FormData
 
 class ListModels(admin.ModelAdmin):
     list_display=['id','name',"email","message","datetime"]
 
 admin.site.register(ContactData,ListModels)
+
+class FOrm
 
 # Register your models here.
