9a00904eab3dfd9f0977c101c57c980e2d4a0058
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-24 20:01:10 +0545

    Initial

--- MyFirstProject/static/js/script.js
+++ MyFirstProject/static/js/script.js
@@ -3,6 +3,7 @@ setInterval(()=>{
     // document.querySelector(".main").style.backgroundColor=`rgb(${color(0,255)},${color(0,255)},${color(0,255)})`
 },100)
 
+console.log("Hello World")
 setTimeout(()=>{
     document.querySelector(".message").textContent=""
 },5000)
