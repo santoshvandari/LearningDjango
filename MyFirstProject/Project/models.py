4437960450084f67c491230ec7adeb67e2b76ca3
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-24 15:17:22 +0545

    Initial

--- MyFirstProject/Project/models.py
+++ MyFirstProject/Project/models.py
@@ -5,6 +5,6 @@ class ContactData(models.Model):
     name=models.CharField(max_length=30)
     email=models.CharField(max_length=30)
     message=models.TextField()
-
+    datetime = models.DateTimeField(auto_now_add = True)
     class Meta:
         db_table="ContactData"
