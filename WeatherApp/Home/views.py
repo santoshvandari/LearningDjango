b929953031456adea21e4a4d51b73ecc2b767417
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-26 20:55:07 +0545

    Initial

--- WeatherApp/Home/views.py
+++ WeatherApp/Home/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+import requests
 
 # Create your views here.
 
 def Index(request):
     if request.method=='POST':
-        pass
+        city = 'london'
+        api_url = 'https://api.api-ninjas.com/v1/weather?city={}'.format(city)
+        response = requests.get(api_url, headers={'X-Api-Key': 'YOUR_API_KEY'})
+        if response.status_code == requests.codes.ok:
+            print(response.text)
+        else:
+            print("Error:", response.status_code, response.text)
     return render(request,"index.html")
