2cb5a35a59d2ae32ff526d03312495ddfaead383
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-26 22:57:20 +0545

    Initial

--- WeatherApp/Home/admin.py
+++ WeatherApp/Home/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Testing
+# from .models import Testing
 # Register your models here.
 
-class TestingDisp(admin.ModelAdmin):
-    list_display = ["id", "name", "email"]
+# class TestingDisp(admin.ModelAdmin):
+#     list_display = ["id", "name", "email"]
 
-admin.site.register(Testing,TestingDisp)
+# admin.site.register(Testing,TestingDisp)
--- WeatherApp/Home/models.py
+++ WeatherApp/Home/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
-# Create your models here.
-class Testing(models.Model):
-    name=models.CharField(max_length=20)
-    email=models.EmailField(max_length=20)
-    class Meta:
-        db_table = "Temporary"
+# # Create your models here.
+# class Testing(models.Model):
+#     name=models.CharField(max_length=20)
+#     email=models.EmailField(max_length=20)
+#     class Meta:
+#         db_table = "Temporary"
