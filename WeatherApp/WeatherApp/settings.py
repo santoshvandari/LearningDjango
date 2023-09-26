be2aa3b1decc721263652920faf38832f2f0f16c
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-26 19:46:12 +0545

    Initial

--- WeatherApp/WeatherApp/settings.py
+++ WeatherApp/WeatherApp/settings.py
@@ -75,18 +75,18 @@ WSGI_APPLICATION = 'WeatherApp.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-     "default": {
-       'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'temp',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',  
-    },
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    #  "default": {
+    #    'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'temp',
+    #     'USER': 'root',
+    #     'PASSWORD': '',
+    #     'HOST': 'localhost',
+    #     'PORT': '3306',  
+    # },
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
