e3f7795ae0cf324f53dd88afa20ccb2954b2fa44
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-26 21:40:22 +0545

    Initial

--- WeatherApp/Home/views.py
+++ WeatherApp/Home/views.py
@@ -8,9 +8,10 @@ def Index(request):
     if request.method=='POST':
         city = 'Kathmandu'
         api_url = f'https://api.api-ninjas.com/v1/weather?city={city}'
-        response = requests.get(api_url, headers={'X-Api-Key': '1455199545msh53bfce68da0845bp10119cjsn23566562dee1'})
+        response = requests.get(api_url, headers={'X-Api-Key': 'U3b+x3/lun8o6+Mm0qYWQw==H7yIc2DshurjNdq7'})
         if response.status_code == requests.codes.ok:
             print(response.text)
+            return render(request,"index.html",response.text)
         else:
             print("Error:", response.status_code, response.text)
     return render(request,"index.html")
