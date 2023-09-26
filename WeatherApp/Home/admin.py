7a3a719211ad36acf409af9bc9230ed680f8f20f
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-26 19:31:46 +0545

    Initial

--- WeatherApp/Home/admin.py
+++ WeatherApp/Home/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from .models import Testing
 # Register your models here.
 
-class TestingDisp()
+class TestingDisp(admin.ModelAdmin):
+    list_display = ["id", "name", "email"]
+    
