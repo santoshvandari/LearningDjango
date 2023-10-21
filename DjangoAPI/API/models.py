a93382f580a5a502acc45e1bd76327b7f7e705e8
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-10-21 20:44:10 +0545

    Initial

--- DjangoAPI/API/models.py
+++ DjangoAPI/API/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class PersonalInformation(models.Model):
-    personalId=models.AutoField(primary_key=True)
-    name=models.CharField(max_length=100)
-    address=models.CharField(max_length=100)
-    description=models.TextField()
-    date=models.DateField(auto_now=True)
-    class Meta:
-        db_table="PersonalInformation",
-        ordering = ["date"]
-
+# class PersonalInformation(models.Model):
+#     personalId=models.AutoField(primary_key=True)
+#     name=models.CharField(max_length=100)
+#     address=models.CharField(max_length=100)
+#     description=models.TextField()
+#     date=models.DateField(auto_now=True)
+#     class Meta:
+#         db_table="PersonalInformation",
+#         ordering = ["date"]
--- DjangoAPI/DjangoAPI/settings.py
+++ DjangoAPI/DjangoAPI/settings.py
@@ -32,8 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     # 'Test',
+    'rest_framework',
     'API',
-    # 'rest_framework'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
