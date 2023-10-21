49165791d68077c059fdd93008c95eaae5ffeff1
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-10-21 20:47:46 +0545

    Initial

--- DjangoAPI/API/views.py
+++ DjangoAPI/API/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Hello World</h1>")
+    return HttpResponse("<h1>Hello Universe</h1>")
