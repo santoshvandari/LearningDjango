9920031c260d74f512f9074bb72680d76c343050
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-10-21 19:26:57 +0545

    Initial

--- DjangoAPI/DjangoAPI/urls.py
+++ DjangoAPI/DjangoAPI/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("santosh/",include("Test.urls")),
-    path('',include('Test.urls')),
+    # path('admin/', admin.site.urls),
+    # path("santosh/",include("Test.urls")),
+    # path('',include('Test.urls')),
 ]
