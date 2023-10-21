3329df61de3d4312187153065d5257f4c2198c81
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-10-21 19:26:00 +0545

    Initial

--- DjangoAPI/Test/views.py
+++ DjangoAPI/Test/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render,HttpResponse
+from django.http import JsonResponse
 
 # Create your views here.
 def santosh(request):
-    return HttpResponse("Hello Santosh")
+    senddata={
+        'name':'Santosh',
+        'age':20,
+        'hobby':'Coding',
+        'address':'Kathmandu',
+        'college':'Kathmandu Engineering College',
+    }
+    # return HttpResponse("Hello Santosh")
+    return JsonResponse(senddata)
