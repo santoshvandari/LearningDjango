c08106043c9df7ac6aea50315b3c3c5fd9cbb61e
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-10-22 10:41:41 +0545

    Initial

--- PostgressSQLLearning/PostgressSQLLearning/settings.py
+++ PostgressSQLLearning/PostgressSQLLearning/settings.py
@@ -73,12 +73,24 @@ WSGI_APPLICATION = 'PostgressSQLLearning.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "E2PNHoF6zi722B7n",
+        "HOST": "db.wcvelnmhgborfrktbfoy.supabase.co",
+        "PORT": "5432",
     }
 }
+
 
 
 # Password validation
