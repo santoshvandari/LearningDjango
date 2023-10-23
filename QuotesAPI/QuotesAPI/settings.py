9d21ac67c4d11cf779d1c20c6cb6496a23537a08
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-10-23 22:14:50 +0545

    Initial

--- QuotesAPI/QuotesAPI/settings.py
+++ QuotesAPI/QuotesAPI/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import dj_database_url
-
+from dotenv import load_dotenv
+import os
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y40_hep_s_9e!%safw3^9is4(!1)u2suhbgau^!klhn_51$p0r'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -86,7 +88,7 @@ WSGI_APPLICATION = 'QuotesAPI.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.config(default='postgresql://postgres:QI4ZlLL4R7YO91rv@db.dsdfemkhffmlomnpghem.supabase.co:5432/postgres')
+    'default': dj_database_url.config(default=os.getenv('DATABASEDETAILS'))
 }
 
 
