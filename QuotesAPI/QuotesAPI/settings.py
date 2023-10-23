c10d7496d9f5a4d3375b274e8907f2ed8e940b4d
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-10-23 18:08:58 +0545

    initial

--- QuotesAPI/QuotesAPI/settings.py
+++ QuotesAPI/QuotesAPI/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'QuotesAPI.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.config(default='postgresql://postgres:[YOUR-PASSWORD]@db.knvjyycyltpldfqjibma.supabase.co:5432/postgres')
+    'default': dj_database_url.config(default='postgresql://postgres:tECbamhozKqu1YmG@db.knvjyycyltpldfqjibma.supabase.co:5432/postgres')
 }
 
 
