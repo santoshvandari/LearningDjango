264b885134ff547362a8a3c99c7d17da653886cc
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-10-23 18:02:09 +0545

    initial

--- QuotesAPI/QuotesAPI/settings.py
+++ QuotesAPI/QuotesAPI/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,12 +76,17 @@ WSGI_APPLICATION = 'QuotesAPI.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='postgres://postgres:[YOUR-PASSWORD]@db.wcvelnmhgborfrktbfoy.supabase.co:5432/postgres')
 }
+
 
 
 # Password validation
