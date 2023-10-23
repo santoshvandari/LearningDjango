39961997933ecc5f046f18cdc0bcf9dff8e2a1d5
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-10-23 18:22:49 +0545

    Initial

--- QuotesAPI/API/views.py
+++ QuotesAPI/API/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from API.models import QuotesCollection
+from API.serilizers import QuotesCollectionSerializer
+
 
 # Create your views here.
+class QuotesCollectionViewSet(viewsets.ModelViewSet):
+    queryset = QuotesCollection.objects.all()
+    serializer_class = QuotesCollectionSerializer
