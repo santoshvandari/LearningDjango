57ff3ff522cf61ce75ec69e18663815aa79a1311
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-10-24 16:29:01 +0545

    Initial

--- QuotesAPI/static/js/script.js
+++ QuotesAPI/static/js/script.js
@@ -1,11 +1,27 @@
-let QuotesRead=()=>{
-    let url=""
-    fetch(url)
-    .then(response=>response.json())
-    .then(data=>{
-        console.log(data)
-    })
-    .catch(err=>{
-        console.log(err)
-    })
-}
+document.getElementById("btn-fetch").addEventListener("click",FetchData);
+// function FetchData(){
+//     console.log("Fetching data");
+//     const url = "api/quotes/";
+//     fetch(url)
+//     .then(response=>response.json())
+//     .then(response=>{
+//         console.log(response)
+//         // document.querySelector("#fetch-result").innerHTML=JSON.stringify(response);
+//         document.querySelector("#fetch-result").textContent=response[0].quotes;
+//         document.querySelector("#author").textContent=" - "+response[0].author;
+//     }).catch(e=>{
+//         console.log(e);
+//     })
+// }
+FetchData();
+
+function FetchData()
+let xhr = new XMLHttpRequest();
+        xhr.open('GET', 'http://randomjokesapi.santosh0.com.np/JokesAPI/');
+        xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            let data = JSON.parse(xhr.responseText);
+            console.log(data);
+            }
+        };
+        xhr.send();
