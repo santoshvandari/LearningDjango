1f5e9aecb5dec6a482f545e5a306df25295af2b0
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-11-12 20:03:43 +0545

    Updated

--- QuotesAPI/QuotesAPI/settings.py
+++ QuotesAPI/QuotesAPI/settings.py
@@ -141,7 +141,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
-        # 'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.JSONRenderer',
         # 'rest_framework.renderers.BrowsableAPIRenderer',
     ]
 }
