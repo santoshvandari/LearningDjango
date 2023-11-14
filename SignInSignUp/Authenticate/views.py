c9a2cf29e0b38e3415582c8b025157e471d8ad93
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-11-14 09:29:57 +0545

    Initial

--- SignInSignUp/Authenticate/views.py
+++ SignInSignUp/Authenticate/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-def 
+def index(request):
+    return render(request,"index.html")
+
+def signin(request):
+    return render(request,"signin.html")
+
+def signup(request):
+    return render(request,"signup.html")
