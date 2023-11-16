b2f77c7d684850deb549a30a95a68c9152d1972b
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-11-16 10:46:58 +0545

    Updated

--- SignInSignUp/Authenticate/views.py
+++ SignInSignUp/Authenticate/views.py
@@ -2,10 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+
+
     return render(request,"index.html")
 
 def signin(request):
     return render(request,"signin.html")
 
 def signup(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        print(name,email,password)
     return render(request,"signup.html")
+
+def logout(request):
+    pass
