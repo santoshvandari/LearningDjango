67a5ebfd5cfd06e1ccc551a68affb507bee01f74
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-11-18 20:31:39 +0545

    Updated

--- StudentManagementSystem/Student/admin.py
+++ StudentManagementSystem/Student/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from Student.models import Student
 
 # Register your models here.
+
+class StudentModel(admin.ModelAdmin):
+    list_display = ["id", "student_number", "first_name", "last_name", "email", "field_of_study", "gpa"]
+    ordering = ["id"]
+
+admin.site.register(Student, StudentModel)
--- StudentManagementSystem/Student/urls.py
+++ StudentManagementSystem/Student/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from Student import views
 
 urlpatterns = [
-    path("",views.index,name="index"),
+    path('', views.index, name='index'),
+    path('<int:id>', views.view_student, name='view_student'),
+    path('add/', views.add, name='add'),
+    path('edit/<int:id>/', views.edit, name='edit'),
+    path('delete/<int:id>/', views.delete, name='delete'),
 ]
