c4f610ec23e846a77b479bb2227ae0dd37be04ed
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-11-18 20:34:03 +0545

    Updated

--- StudentManagementSystem/migrate/apps.py
+++ StudentManagementSystem/migrate/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MakemigrationsConfig(AppConfig):
+class MigrateConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'makemigrations'
+    name = 'migrate'
