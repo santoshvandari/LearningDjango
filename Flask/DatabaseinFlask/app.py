ec3f3cf38f3978e59fec54639c8291a2ed307cc9
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-03-10 22:07:03 +0545

    Initial

--- Flask/DatabaseinFlask/app.py
+++ Flask/DatabaseinFlask/app.py
@@ -1,11 +1,20 @@
 from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
 
-
-# db=SQLAlchemy(model_class=Base)
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///database.db"
-# db.init_app(app)
+db=SQLAlchemy(app)
+
+class StudentInfo(db.Model):
+    rollno=db.Column(db.Integer, primary_key=True)
+    fullname=db.Column(db.String(50),nullable=False)
+    Address=db.Column(db.String(70),nullable=False)
+    
+    def __repr__(self) -> str:
+        return f"{self.rollno},{self.fullname},{self.Address}"
+    
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def home():
