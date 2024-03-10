e82f0ea14b02c1afbeb035e53eeaa14ce156ef7e
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-03-10 08:18:17 +0545

    Initial

--- Flask/TemplatesDefine/app.py
+++ Flask/TemplatesDefine/app.py
@@ -6,5 +6,9 @@ app=flask.Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/")
+def about():
+    return render_template('about.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
