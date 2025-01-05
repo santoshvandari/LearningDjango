728bff94cfa92e39920dd5e344e783dad7a8f597
Santosh Bhandari <santoshvandari1@gmail.com>
2025-01-05 14:18:49 +0545

    FastAPI Auth System Learning

--- FastAPI/FastAPIAuth/SecondExample/main.py
+++ FastAPI/FastAPIAuth/SecondExample/main.py
@@ -1,19 +1,64 @@
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
-from users.routes import router as guest_router, user_router
-from auth.route import router as auth_router
-from core.security import JWTAuth
-from starlette.middleware.authentication import AuthenticationMiddleware
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from datetime import timedelta
+from fastapi.middleware.cors import CORSMiddleware
+from database import get_db
+from services import authenticate_user, create_access_token, get_user_by_username, create_user, verify_token
+from sqlalchemy.orm import Session
+from model import UserCreate
+
 
 
 app = FastAPI()
-app.include_router(guest_router)
-app.include_router(user_router)
-app.include_router(auth_router)
 
-# Add Middleware
-app.add_middleware(AuthenticationMiddleware, backend=JWTAuth())
 
-@app.get('/')
-def health_check():
-    return JSONResponse(content={"status": "Running!"})
+
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins from the list
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+
+
+
+
+@app.post("/register")
+def register_user(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = get_user_by_username(db, username=user.username)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Username already registered")
+    return create_user(db=db, user=user)
+
+
+
+@app.post("/token")
+def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+
+
+@app.get("/verify-token/{token}")
+async def verify_user_token(token: str):
+    verify_token(token=token)
+    return {"message": "Token is valid"}
---
+++ FastAPI/FastAPIAuth/SecondExample/services.py
@@ -0,0 +1,59 @@
+from datetime import datetime, timedelta
+from fastapi import HTTPException, Depends
+from fastapi.security import OAuth2PasswordBearer
+from passlib.context import CryptContext
+from jose import JWTError, jwt
+from db_model import User
+from sqlalchemy.orm import Session
+from model import UserCreate
+
+
+
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# Your JWT secret and algorithm
+SECRET_KEY = "your_secret_key"
+ALGORITHM = "HS256"
+
+def get_user_by_username(db: Session, username: str):
+    return db.query(User).filter(User.username == username).first()
+
+def create_user(db: Session, user: UserCreate):
+    hashed_password = pwd_context.hash(user.password)
+    db_user = User(username=user.username, hashed_password=hashed_password)
+    db.add(db_user)
+    db.commit()
+    return "complete"
+
+# Authenticate the user
+def authenticate_user(username: str, password: str, db: Session):
+    user = db.query(User).filter(User.username == username).first()
+    if not user:
+        return False
+    if not pwd_context.verify(password, user.hashed_password):
+        return False
+    return user
+
+# Create access token
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def verify_token(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise HTTPException(status_code=403, detail="Token is invalid or expired")
+        return payload
+    except JWTError:
+        raise HTTPException(status_code=403, detail="Token is invalid or expired")
