791b07260c829e539996068128d65489cc4726e5
Santosh Bhandari <santoshvandari1@gmail.com>
2025-01-11 19:06:34 +0545

    Configuring

--- Django/Learning/main/views.py
+++ Django/Learning/main/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return JsonResponse({"message":"Hello World!"})
+    # return JsonResponse({"message":"Hello World!"})
+    return render(request,"index.html")
