cd93b2091be39ce32b275b549cdffb77853d86b6
Santosh Bhandari <santoshvandari1@gmail.com>
2025-01-16 10:59:30 +0545

    Initial

--- Docker/Example1/core/settings.py
+++ Docker/Example1/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)w%o#or4@dka9^^58dqe1#0o1fv(b*j+*kv$mf4uqck(fx)z__
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
